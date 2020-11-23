55339678109d57be4832726569e8131efccadaed
Techoplite <oriccimirko@gmail.com>
2020-11-23 14:02:10 +0000

    Initialise with Mongo DB connected to Express

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully...");
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}...`);
+});
